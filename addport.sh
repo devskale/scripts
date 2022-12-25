02a9e25cb87e169acc28d2aee5ae3775e6cf9407
fg@amd <flaggrgastr@gmail.com>
2022-12-25 22:58:37 +0100

    addport arguments

--- addport.sh
+++ addport.sh
@@ -1,3 +1,22 @@
-sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
+#!/bin/bash
 
-sudo netfilter-persistent save
+#if [ $# -eq 0 ]; then
+# echo "no arguments"
+# exit 1
+#fi
+
+if [ -z "$1" ]
+  then
+    echo "Add port as argument"
+    echo "addport PORTNUM"
+    exit 1
+fi
+
+PORT=$1
+
+sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport $PORT -j ACCEPT
+
+sudo netfilter-persistent save &>/dev/null
+
+echo "added Port $PORT"
+
