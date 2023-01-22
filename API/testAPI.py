a8c0dad94771c3760b58f0acce9c5ea1419f0e1f
dev skale.io <dev@skale.io>
2023-01-22 17:12:34 +0100

    update

--- API/testAPI.py
+++ API/testAPI.py
@@ -1,6 +1,8 @@
 import requests
 
 response = requests.get('http://localhost:5000/tasks/2')
+#response = requests.get('https://burgenland2021.org:5000/tasks/2')
+#response = requests.get('https://woodmastr.gotdns.ch:5000/tasks/2')
 
 if response.status_code == 200:
     print(response.json())
