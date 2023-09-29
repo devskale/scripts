1fb444651bb626d06c89a479b64ed6ea0206489c
fg@amd <flaggrgastr@gmail.com>
2023-09-29 08:13:43 +0200

    updates

--- API/testAPI.py
+++ API/testAPI.py
@@ -1,10 +1,36 @@
 import requests
+import json
 
-response = requests.get('http://localhost:5000/tasks/2')
-#response = requests.get('https://burgenland2021.org:5000/tasks/2')
-#response = requests.get('https://woodmastr.gotdns.ch:5000/tasks/2')
+post_url = "https://amd2.mooo.com/api/tasks"
+#get_url = "https://amd2.mooo.com/api/tasks/"
 
-if response.status_code == 200:
-    print(response.json())
+data = {
+    "title": "Learn Python",
+    "description": "Learn the basics of Python programming",
+    "done": False
+}
+
+headers = {
+    'Content-Type': 'application/json'
+}
+
+# Test POST endpoint
+postresponse = requests.post(post_url, data=json.dumps(data), headers=headers)
+
+if postresponse.status_code == 201:
+    book = postresponse.json()['task']
+    print("Success: Book added successfully. Book data:", book)
 else:
-    print('Error:', response.status_code)
+    print("Error: Failed to add book. Status code:", postresponse.status_code)
+
+task_id = postresponse.json()['task']['id']
+
+# Test GET endpoint
+get_url = f"https://amd2.mooo.com/api/tasks/{task_id}"
+getresponse = requests.get(get_url)
+
+if getresponse.status_code == 200:
+    book = getresponse.json()['task']
+    print("Success: Book retrieved successfully. Book data:", book)
+else:
+    print("Error: Failed to retrieve book. Status code:", getresponse.status_code)
--- API/testAPI0.py
+++ API/testAPI0.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get('http://localhost:5000/tasks/2')
+response = requests.get('http://amd2.mooo.com/api/tasks/2')
 
 if response.status_code == 200:
     print(response.json())
